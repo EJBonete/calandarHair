a86d0312264f9646d28ec529210d6ce9e2ae1c19
MarcMonterde <clanbil@gmail.com>
2022-04-19 20:10:47 +0200

    loginactivity referenciado

--- app/src/main/java/com/proyecto/IFP/LoginActivity.java
+++ app/src/main/java/com/proyecto/IFP/LoginActivity.java
@@ -16,17 +16,20 @@ public class LoginActivity extends AppCompatActivity {
     protected EditText box2; // pwd
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
         label1 = (TextView) findViewById((R.id.label_loggin));
         label2 = (TextView) findViewById((R.id.label1_loggin));
         box1 = (EditText) findViewById(R.id.box_loggin);
         box2 = (EditText) findViewById(R.id.box1_loggin);
+
+
+
 
 
     }
