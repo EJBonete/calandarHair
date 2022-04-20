0e98c97aeeea7c468b63c3bcc15598ad9f716892
MarcMonterde <clanbil@gmail.com>
2022-04-20 08:46:13 +0200

    loginactivity referenciado cambiado nombre variables

--- app/src/main/java/com/proyecto/IFP/LoginActivity.java
+++ app/src/main/java/com/proyecto/IFP/LoginActivity.java
@@ -12,8 +12,8 @@ public class LoginActivity extends AppCompatActivity {
 
     protected TextView label1;
     protected TextView label2;
-    protected EditText box1; // user
-    protected EditText box2; // pwd
+    protected EditText box; // user
+    protected EditText box1; // pwd
 
 
 
@@ -25,8 +25,8 @@ public class LoginActivity extends AppCompatActivity {
 
         label1 = (TextView) findViewById((R.id.label_loggin));
         label2 = (TextView) findViewById((R.id.label1_loggin));
-        box1 = (EditText) findViewById(R.id.box_loggin);
-        box2 = (EditText) findViewById(R.id.box1_loggin);
+        box = (EditText) findViewById(R.id.box_loggin);
+        box1 = (EditText) findViewById(R.id.box1_loggin);
 
 
 
