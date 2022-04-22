2d8eeb1399e37a934adbe8fa30bc24dbc9f0c3e6
MarcMonterde <clanbil@gmail.com>
2022-04-22 16:49:09 +0200

    Commint Marc Viernes

--- app/src/main/java/com/proyecto/IFP/AccesoActivity.java
+++ app/src/main/java/com/proyecto/IFP/AccesoActivity.java
@@ -2,9 +2,20 @@ package com.proyecto.IFP;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AccesoActivity extends AppCompatActivity {
+
+
+    protected Button boton1; // loguin
+    protected Button boton2; // registro
+    private Intent pasarPantalla;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
