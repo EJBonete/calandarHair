57a83dbb138a150e09c8ea65c2a112206d011c7c
Nacho <marzanigml@gmail.com>
2022-04-25 19:17:11 +0200

    Añado botón 2

--- app/src/main/java/com/proyecto/IFP/NuevaCitaActivity.java
+++ app/src/main/java/com/proyecto/IFP/NuevaCitaActivity.java
@@ -13,9 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-
-
-public class NuevaCitaActivity extends AppCompatActivity  {
+public class NuevaCitaActivity extends AppCompatActivity {
 
 
     protected Button boton1; // atras
@@ -27,15 +25,14 @@ public class NuevaCitaActivity extends AppCompatActivity  {
     private Intent pasarPantalla;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_cita);
 
 
-        boton1= (Button) findViewById(R.id.button1_Nueva);
-        boton2= (Button) findViewById(R.id.button2_Nueva);
+        boton1 = (Button) findViewById(R.id.button1_Nueva);
+        boton2 = (Button) findViewById(R.id.button2_Nueva);
         nombre = (EditText) findViewById(R.id.EditText1_Nueva);
         trabajo = (EditText) findViewById(R.id.EditText2_Nueva);
         otros = (EditText) findViewById(R.id.EditText3_Nueva);
@@ -46,6 +43,28 @@ public class NuevaCitaActivity extends AppCompatActivity  {
                 pasarPantalla = new Intent(NuevaCitaActivity.this, AccesoActivity.class);
                 finish();
                 startActivity(pasarPantalla);
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!nombre.getText().toString().isEmpty() && !trabajo.getText().toString().isEmpty() /*&& !telefono.getText().toString().isEmpty()*/) {
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE, nombre.getText().toString());
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, trabajo.getText().toString());
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION, otros.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, false);
+                    intent.putExtra(Intent.EXTRA_EMAIL, "clanbil@gmail.com, marcmonterdemolina@gmail.com");
+
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(NuevaCitaActivity.this, "Acción no soportada", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(NuevaCitaActivity.this, "Debes rellenar los datos Nombre y Trabajo", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
