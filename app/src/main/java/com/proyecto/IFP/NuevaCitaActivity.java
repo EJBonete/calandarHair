5c3e806dcadfc38d89a43c84860495ccd8965572
MarcMonterde <clanbil@gmail.com>
2022-04-25 11:41:08 +0200

    Rama Marc . 1.- Activity Nueva cita hecha UI y agregada funcion Google calendar

--- app/src/main/java/com/proyecto/IFP/NuevaCitaActivity.java
+++ app/src/main/java/com/proyecto/IFP/NuevaCitaActivity.java
@@ -1,25 +1,44 @@
 package com.proyecto.IFP;
 
+// Video usado: https://www.youtube.com/watch?v=NK_-phxyIAM
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class NuevaCitaActivity extends AppCompatActivity {
+
+
+
+public class NuevaCitaActivity extends AppCompatActivity  {
 
 
     protected Button boton1; // atras
+    protected Button boton2; // Añadir
+    protected EditText nombre;
+    protected EditText trabajo;
+    protected EditText otros;
+
     private Intent pasarPantalla;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_citas_proximas);
+        setContentView(R.layout.activity_nueva_cita);
+
 
         boton1= (Button) findViewById(R.id.button1_Nueva);
-
+        boton2= (Button) findViewById(R.id.button2_Nueva);
+        nombre = (EditText) findViewById(R.id.EditText1_Nueva);
+        trabajo = (EditText) findViewById(R.id.EditText2_Nueva);
+        otros = (EditText) findViewById(R.id.EditText3_Nueva);
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +46,31 @@ public class NuevaCitaActivity extends AppCompatActivity {
                 pasarPantalla = new Intent(NuevaCitaActivity.this, AccesoActivity.class);
                 finish();
                 startActivity(pasarPantalla);
+            }
+        });
+        
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!nombre.getText().toString().isEmpty() && !trabajo.getText().toString().isEmpty() /*&& !telefono.getText().toString().isEmpty()*/)
+                {
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE, nombre.getText().toString());
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, trabajo.getText().toString());
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION, otros.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, false);
+                    intent.putExtra(Intent.EXTRA_EMAIL, "clanbil@gmail.com, marcmonterdemolina@gmail.com");
+
+                    if(intent.resolveActivity(getPackageManager()) !=null){
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(NuevaCitaActivity.this, "Acción no soportada", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(NuevaCitaActivity.this, "Debes rellenar los datos Nombre y Trabajo", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
