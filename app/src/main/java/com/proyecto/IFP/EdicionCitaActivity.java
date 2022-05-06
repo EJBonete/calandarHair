a85b004a0d5159de09e19e7da1af0d64402afb8c
MarcMonterde <clanbil@gmail.com>
2022-05-06 18:44:54 +0200

    capturamos datos del listview para editar

--- app/src/main/java/com/proyecto/IFP/EdicionCitaActivity.java
+++ app/src/main/java/com/proyecto/IFP/EdicionCitaActivity.java
@@ -15,13 +15,35 @@ public class EdicionCitaActivity extends AppCompatActivity {
     private Button boton2;
     private Button boton3;
     private Intent pasarPantalla;
+    private Bundle extra;
+    private String datoNombre="";
+    private String datoTelefono="";
+    private String datoServicio="";
+    private String datoFecha="";
+    private String datoFecha="";
+    private String datoHora="";
+    private String datoObservaciones="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_edicion_cita);
+        // Elimina barra superior UI
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        extra = getIntent().getExtras();
+        if( extra != null){
+            datoNombre= extra.getString("nombre");
+            datoTelefono= extra.getString("telefono");
+            datoServicio= extra.getString("servicio");
+            datoFecha= extra.getString("fecha");
+            datoHora= extra.getString("hora");
+            datoObservaciones= extra.getString("observaciones");
+
+
+        }
 
         boton1 = (Button) findViewById(R.id.button_1_back_edicionCitas);
         boton2 = (Button) findViewById(R.id.button_2_save_nuevacita);
